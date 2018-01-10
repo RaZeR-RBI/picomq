7507bc7c8eee44edf3d6a8e2c657a15140027bc6
Adel Vilkov <vilkov.adel@gmail.com>
2018-01-10 19:41:06 +0300

    read_packet (read_var_header not implemented)

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -38,7 +38,7 @@ pub struct FixedHeader {
     qos: QoS,
     retain: bool,
     remaining_bytes: u32,
-    payload_offset: u8,
+    payload_offset: usize,
 }
 
 #[derive(Debug, PartialEq)]
@@ -112,7 +112,7 @@ impl ConnAckHeader {
     }
 }
 
-fn read_packet_type(b: &u8) -> PacketType {
+fn read_packet_type(b: u8) -> PacketType {
     let value = (b & 0xF0) >> 4;
     match value {
         1 => PacketType::Connect,
@@ -157,10 +157,10 @@ fn vlq(bytes: &Bytes) -> (u32, usize) {
     }
 }
 
-fn read_header(bytes: Bytes) -> FixedHeader {
-    let byte_1 = &bytes[0];
+fn read_header(bytes: &Bytes) -> FixedHeader {
+    let byte_1 = bytes[0];
     let ptype = read_packet_type(byte_1);
-    let mut payload_offset = 1u8;
+    let mut payload_offset = 1;
 
     let dup = (byte_1 & 0x08) == 8;
     let qos = match (byte_1 & 0x06) >> 1 {
@@ -172,7 +172,7 @@ fn read_header(bytes: Bytes) -> FixedHeader {
     let retain = (byte_1 & 0x01) == 1;
 
     let (remaining_bytes, offset) = vlq(&bytes.slice_from(1));
-    payload_offset += offset as u8;
+    payload_offset += offset;
     FixedHeader {
         packet_type: ptype,
         dup: dup,
@@ -180,6 +180,22 @@ fn read_header(bytes: Bytes) -> FixedHeader {
         retain: retain,
         remaining_bytes: remaining_bytes,
         payload_offset: payload_offset,
+    }
+}
+
+fn read_var_header(header: &FixedHeader, bytes: &Bytes) -> (TypeHeader, usize) {
+    panic!("Not implemented")
+}
+
+pub fn read_packet(bytes: Bytes) -> MqttPacket {
+    let header = read_header(&bytes);
+    let fixed_offset = header.payload_offset;
+    let (var_header, var_offset) = read_var_header(&header, &bytes.slice_from(fixed_offset));
+    let payload = bytes.slice_from(fixed_offset + var_offset);
+    MqttPacket {
+        header: header,
+        var_header: var_header,
+        payload: payload,
     }
 }
 
@@ -213,7 +229,7 @@ mod tests {
             .collect();
 
         for (byte, ref ptype) in type_map.iter() {
-            assert_eq!(read_packet_type(byte), **ptype);
+            assert_eq!(read_packet_type(*byte), **ptype);
         }
     }
 
@@ -240,7 +256,7 @@ mod tests {
     #[test]
     fn reads_header_without_packet_id() {
         let connect_command = Bytes::from(vec![0x10, 0x25]);
-        let header = read_header(connect_command);
+        let header = read_header(&connect_command);
         assert_eq!(header.packet_type, PacketType::Connect);
         assert_eq!(header.remaining_bytes, 37);
 
@@ -253,7 +269,7 @@ mod tests {
     #[test]
     fn reads_header_with_qos_and_packet_id() {
         let subscribe_command = Bytes::from(vec![0x82, 0x10, 0x00, 0x01]);
-        let header = read_header(subscribe_command);
+        let header = read_header(&subscribe_command);
         assert_eq!(header.packet_type, PacketType::Subscribe);
         assert_eq!(header.remaining_bytes, 16);
         assert_eq!(header.dup, false);
